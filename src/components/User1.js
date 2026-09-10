fc87649ba24cb7c02b5416fdca10ca9e32ce4d9a
unknown <govindsharmags1057@gmail.com>
2026-09-10 15:23:25 +0530

    updated app

--- src/components/User1.js
+++ src/components/User1.js
@@ -3,24 +3,25 @@ import 'regenerator-runtime/runtime'
 const User1 = (props) => {
    const id = props.match.params.id;
    let [user, setUser] = useState([]);
+   let [loading, setLoading] = useState(true);
    let getdata = async () => {
       let response = await fetch('https://jsonplaceholder.typicode.com/users');
-
       let res = await response.json();
-
       setUser(res);
+      setLoading(false);
    };
 
    useEffect(() => {
       getdata();
    }, []);
+   if (loading) {
+   return <div>Loading...</div>;
+}
 
    let userfind = user.find((item) => item.id == id);
 
    console.log(userfind);
-       if (!userfind) {
-    return <div>Loading...</div>;
-  }
+   
 
    return (
       <div>   
